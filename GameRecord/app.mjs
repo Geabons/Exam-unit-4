b274ce9502e80dd8ee3a30eda6a9ca40f01ae8fe
Geabons <Geabonzo@gmail.com>
2025-04-11 23:01:35 +0200

    step 4

--- GameRecord/app.mjs
+++ GameRecord/app.mjs
@@ -3,18 +3,31 @@ import game from "./models/Game.mjs";
 let storedGames = [];
 const gameKey = "";
 
-function saveGameToStorage(gameObject){
-    localStorage.setItem(gameKey, importJSONToString(gameObject));
+function saveGameToStorage(gameObject) {
+  localStorage.setItem(gameKey, importJSONToString(gameObject));
 }
 
-function loadGameFromStorage(){
-    return localStorage.getItem(gameKey);
+function loadGameFromStorage() {
+  return localStorage.getItem(gameKey);
 }
 
-function retrieveGameFromStorage(){
-    return JSON.parse(loadGameFromStorage());
+function retrieveGameFromStorage() {
+  return JSON.parse(loadGameFromStorage());
 }
 
-function importJSONToString(JSONString){
-    return JSON.stringify(JSONString);
+function importJSONToString(JSONString) {
+  return JSON.stringify(JSONString);
 }
+
+document.getElementById("importSource").onchange = function (e) {
+  const file = e.target.files[0];
+  let fileReader = new FileReader();
+  fileReader.onload = function (event) {
+    saveGameToStorage(event.target.result);
+    //visualRecord
+  };
+  fileReader.onerror = function (event) {
+    console.error("Error with reading file: ", event.target.error);
+  };
+  fileReader.readAsText(file);
+};
