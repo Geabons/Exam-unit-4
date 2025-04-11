f00c23c381471914d890fba2a69fa7b934681f47
Geabons <Geabonzo@gmail.com>
2025-04-11 22:47:03 +0200

    step 3 + fix to step 1

--- GameRecord/app.mjs
+++ GameRecord/app.mjs
@@ -1,1 +1,20 @@
 import game from "./models/Game.mjs";
+
+let storedGames = [];
+const gameKey = "";
+
+function saveGameToStorage(gameObject){
+    localStorage.setItem(gameKey, importJSONToString(gameObject));
+}
+
+function loadGameFromStorage(){
+    return localStorage.getItem(gameKey);
+}
+
+function retrieveGameFromStorage(){
+    return JSON.parse(loadGameFromStorage());
+}
+
+function importJSONToString(JSONString){
+    return JSON.stringify(JSONString);
+}
